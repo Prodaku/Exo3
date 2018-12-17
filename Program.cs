6d466d67d5ed4bd1074519d36338c4c4837d6cec
Prodaku <b.isaacpro@gmail.com>
2018-12-17 14:31:08 +0100

    Exo en cours pas totalement fini

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "1";
+            string b = "2";
+            Console.WriteLine("Veuillez renseigner les nombres suivants : " a + " " + b);
+        }
+    }
+}
